1877fab5e7479511459b0bac576391286df73cab
Saul Zepeda <saul.zepeda@toshibagcs.com>
2022-02-27 18:29:15 -0600

    numbers file added and now it removes the line break.

--- tokenizer/file.py
+++ tokenizer/file.py
@@ -1,5 +1,5 @@
 res_words = ['if', 'for', 'while', 'else', 'int', 'float', 'bool', '#include', '#define', '<iostream>', 
-'cout', 'cin', 'return', 'main', 'using', 'namespace', 'std']
+'cout', 'cin', 'return', 'main', 'using', 'namespace', 'std', '<<', '>>']
 op_arith = ['+', '-', '/', '*', '^']
 op_relat = ['<', '>', '<=', '>=']
 op_logic = ['&&', '||']
@@ -11,16 +11,20 @@ with open('src.txt', 'r') as file:
 f_res = open("res_words.txt", "w")
 f_arith = open("op_arith.txt", "w")
 f_relat = open("op_relat.txt", "w")
-f_logic = open('op_logic', 'w')
-f_grouping = open('op_grouping', 'w')
-f_ids = open('ids', 'w')
+f_logic = open('op_logic.txt', 'w')
+f_grouping = open('op_grouping.txt', 'w')
+f_ids = open('ids.txt', 'w')
+f_nums = open('nums.txt', 'w')
 
 for line in lines:
+    line = line[:-1]
+
     words = line.split(' ')
     for word in words:
-        if word == '\n' or word == ' ':
-            break
-        if word in res_words:
+        if word.endswith(';'):
+            word = word[:-1]
+
+        elif word in res_words:
             f_res.write(word + '\n')
         elif word in op_arith:
             f_arith.write(word + '\n')
@@ -30,6 +34,8 @@ for line in lines:
             f_logic.write(word + '\n')
         elif word in op_grouping:
             f_grouping.write(word + '\n')
+        elif word.isnumeric():
+            f_nums.write(word + '\n')
         elif len(word) > 0:
             f_ids.write(word + '\n')
 
@@ -39,3 +45,4 @@ f_relat.close()
 f_logic.close()
 f_grouping.close()
 f_ids.close()
+f_nums.close()
